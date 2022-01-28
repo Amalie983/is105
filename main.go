816de4308121d91ad218e6feb89753fed1d51fe4
Amalie983 <amalieel@uia.no>
2022-01-28 13:56:49 +0100

    fjernet no greier og la til no greier

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/Amalie983/is105/myquote"
 
 )
 
 
-func main()  {                           // for å komme i gang fikk jeg informasjon fra: https://go.dev/doc/tutorial/getting-started
-	fmt.Println(myquote.First())        // for å printe alt til skjermen sjekket jeg: https://pkg.go.dev/rsc.io/quote/v4
+func main()  {
 	fmt.Println(myquote.Second())
 	fmt.Println(myquote.Third())
 	fmt.Println(myquote.Fourth())
+	fmt.Println(myquote.First())
 }
